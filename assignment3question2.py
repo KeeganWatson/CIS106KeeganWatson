e657b78af7adcc9f2938d1638479eb88e35e4460
KeeganWatson <79818234+KeeganWatson@users.noreply.github.com>
2021-03-17 21:00:47 -0500

    Add files via upload

---
+++ assignment3question2.py
@@ -0,0 +1,6 @@
+price = float(input("Enter stock price"))
+qty = float(input("Enter quantity of stock"))
+
+v = qty * price
+
+print("Current value",v)
---
+++ cis106assignment3question1.py
@@ -0,0 +1,6 @@
+score1 = float(input("Enter score 1"))
+score2 = float(input("Enter score 2"))
+sixty = float(.6)
+fourty = float(.4)
+fnlscore = score1 * sixty + score2 * fourty
+print ("Final score   ",fnlscore)
